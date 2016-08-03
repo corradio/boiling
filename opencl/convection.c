4ed888324ff190ae3a70eeca687a23b4955a00f7
Olivier Corradi <olivier.corradi@gmail.com>
2016-08-03 14:01:51 +0200

    Clean up

--- opencl/convection.c
+++ opencl/convection.c
@@ -1,55 +1,3 @@
-//
-// File:       hello.c
-//
-// Abstract:   A simple "Hello World" compute example showing basic usage of OpenCL which
-//             calculates the mathematical square (X[i] = pow(X[i],2)) for a buffer of
-//             floating point values.
-//             
-//
-// Version:    <1.0>
-//
-// Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple Inc. ("Apple")
-//             in consideration of your agreement to the following terms, and your use,
-//             installation, modification or redistribution of this Apple software
-//             constitutes acceptance of these terms.  If you do not agree with these
-//             terms, please do not use, install, modify or redistribute this Apple
-//             software.
-//
-//             In consideration of your agreement to abide by the following terms, and
-//             subject to these terms, Apple grants you a personal, non - exclusive
-//             license, under Apple's copyrights in this original Apple software ( the
-//             "Apple Software" ), to use, reproduce, modify and redistribute the Apple
-//             Software, with or without modifications, in source and / or binary forms;
-//             provided that if you redistribute the Apple Software in its entirety and
-//             without modifications, you must retain this notice and the following text
-//             and disclaimers in all such redistributions of the Apple Software. Neither
-//             the name, trademarks, service marks or logos of Apple Inc. may be used to
-//             endorse or promote products derived from the Apple Software without specific
-//             prior written permission from Apple.  Except as expressly stated in this
-//             notice, no other rights or licenses, express or implied, are granted by
-//             Apple herein, including but not limited to any patent rights that may be
-//             infringed by your derivative works or by other works in which the Apple
-//             Software may be incorporated.
-//
-//             The Apple Software is provided by Apple on an "AS IS" basis.  APPLE MAKES NO
-//             WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION THE IMPLIED
-//             WARRANTIES OF NON - INFRINGEMENT, MERCHANTABILITY AND FITNESS FOR A
-//             PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND OPERATION
-//             ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
-//
-//             IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL OR
-//             CONSEQUENTIAL DAMAGES ( INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-//             SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-//             INTERRUPTION ) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION, MODIFICATION
-//             AND / OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED AND WHETHER
-//             UNDER THEORY OF CONTRACT, TORT ( INCLUDING NEGLIGENCE ), STRICT LIABILITY OR
-//             OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-// Copyright ( C ) 2008 Apple Inc. All Rights Reserved.
-//
- 
-////////////////////////////////////////////////////////////////////////////////
- 
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -61,18 +9,10 @@
 #include <sys/time.h>
 #include <OpenCL/opencl.h>
 
-struct timeval tv;
- 
 ////////////////////////////////////////////////////////////////////////////////
- 
-// Use a static data size for simplicity
-//
-#define DATA_SIZE (1024000)
- 
-////////////////////////////////////////////////////////////////////////////////
- 
-// Simple compute kernel which computes the square of an input array 
-//
+
+#define DATA_SIZE (1024)
+
 const char *KernelSource = "\n" \
 "__kernel void square(                                                       \n" \
 "   __global float* input,                                              \n" \
@@ -84,8 +24,6 @@ const char *KernelSource = "\n" \
 "       output[i] = input[i] * input[i];                                \n" \
 "}                                                                      \n" \
 "\n";
- 
-////////////////////////////////////////////////////////////////////////////////
  
 int main(int argc, char** argv)
 {
@@ -141,7 +79,7 @@ int main(int argc, char** argv)
         printf("Error: Failed to create a command commands!\n");
         return EXIT_FAILURE;
     }
- 
+    
     // Create the compute program from the source buffer
     //
     program = clCreateProgramWithSource(context, 1, (const char **) & KernelSource, NULL, &err);
@@ -217,8 +155,6 @@ int main(int argc, char** argv)
     // Execute the kernel over the entire range of our 1d input data set
     // using the maximum number of work group items for this device
     //
-    gettimeofday(&tv,NULL);
-    printf("%d\n", tv.tv_usec);
     global = count;
     err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
     if (err)
@@ -240,8 +176,6 @@ int main(int argc, char** argv)
         exit(1);
     }
     
-    gettimeofday(&tv,NULL);
-    printf("%d\n", tv.tv_usec);
     // Validate our results
     //
     correct = 0;
@@ -251,8 +185,6 @@ int main(int argc, char** argv)
             correct++;
     }
     
-    gettimeofday(&tv,NULL);
-    printf("%d\n", tv.tv_usec);
     
     // Print a brief summary detailing the results
     //
